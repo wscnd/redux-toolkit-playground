e46c035f96e501b8ffb5e98e0effe4a001545e6c
wscnd <wellingson@gmail.com>
2021-06-12 21:07:43 -0300

    Reducer with Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './features/counter/counter-slice';
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  function handleClick() {
+    // increment by 1
+    // dispatch(incremented());
+
+    // increment by a fixed amount
+    dispatch(amountAdded(3));
+  }
 
   return (
     <div className="App">
@@ -11,13 +22,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button onClick={handleClick}>
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+        
         <p>
           <a
             className="App-link"
